8576a785b70c4181111afe85741cffba510a8070
linstevenyy <c93yo6123@hotmail.com>
2016-03-21 10:16:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/linstevenyy/android_github/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.linstevenyy.android_github;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wv;
+    TextView tv;
+
+    WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv = (WebView) findViewById(R.id.webView);
+        tv = (TextView) findViewById(R.id.textView);
+        wv.setWebViewClient(mWebViewClient);
+        WebSettings webSettings = wv.getSettings();
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        wv.loadUrl("http://tw.yahoo.com");
+    }
+}
